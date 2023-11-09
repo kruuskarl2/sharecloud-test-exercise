313fa231dcb2a7764d37454fad90190621c5033d
kruuskarl2 <karlkruus0@gmail.com>
2023-11-09 13:04:31 +0200

    Add task limit

--- src/webparts/sharecloudTestExercise/components/TaskTable/NewTaskButton/NewTaskButton.tsx
+++ src/webparts/sharecloudTestExercise/components/TaskTable/NewTaskButton/NewTaskButton.tsx
@@ -1,5 +1,6 @@
 import { Suspense, lazy, useState } from 'react';
 import classes from './NewTaskButton.module.scss';
+import { useTaskContext } from '../../../context/TaskContext';
 
 const TaskDialog = lazy(
     () =>
@@ -11,6 +12,8 @@ const TaskDialog = lazy(
 const NewTaskButton: React.FC = () => {
     const [isOpen, setIsOpen] = useState(false);
 
+    const { tasks } = useTaskContext();
+
     const onOpen = (): void => {
         setIsOpen(true);
     };
@@ -19,9 +22,15 @@ const NewTaskButton: React.FC = () => {
         setIsOpen(false);
     };
 
+    const isDisabled = tasks.length >= 10;
+
     return (
         <>
-            <button className={classes.root} onClick={onOpen} />
+            <button
+                className={classes.root}
+                onClick={onOpen}
+                disabled={isDisabled}
+            />
             <Suspense fallback={null}>
                 {isOpen ? (
                     <TaskDialog
